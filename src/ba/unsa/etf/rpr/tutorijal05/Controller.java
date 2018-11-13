16d75cb7ca819ee9299d15809e7adedb07aab930
aimamovic5 <44173127+aimamovic5@users.noreply.github.com>
2018-11-13 23:24:34 +0100

    gui

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,15 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
 
+
+    public void numberClick(ActionEvent actionEvent) {
+
+    }
+
+    public void operationClick(ActionEvent actionEvent) {
+
+    }
 }
